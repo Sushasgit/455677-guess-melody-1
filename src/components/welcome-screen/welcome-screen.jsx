7c42216b7f7a115de07837dd8944d5ab1222519e
Sushasgit <gryshchukoleksandra@gmail.com>
2019-05-03 19:38:57 +0300

    Added proptypes validation

--- src/components/welcome-screen/welcome-screen.jsx
+++ src/components/welcome-screen/welcome-screen.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const WelcomeScreen = () => {
+WelcomeScreen.propTypes = {
+  time: PropTypes.number,
+  mistakes: PropTypes.number
+};
+
+const WelcomeScreen = (props) => {
   return <section className="welcome">
     <div className="welcome__logo">
       <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83"/>
@@ -9,8 +15,8 @@ const WelcomeScreen = () => {
     <h2 className="welcome__rules-title">Правила игры</h2>
     <p className="welcome__text">Правила просты:</p>
     <ul className="welcome__rules-list">
-      <li>За 5 минут нужно ответить на все вопросы.</li>
-      <li>Можно допустить 3 ошибки.</li>
+      <li>За {props.time} минут нужно ответить на все вопросы.</li>
+      <li>Можно допустить {props.mistakes} ошибки.</li>
     </ul>
     <p className="welcome__text">Удачи!</p>
   </section>;
